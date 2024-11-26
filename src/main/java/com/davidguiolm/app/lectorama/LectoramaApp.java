af39b5918d6257495e2de9117259b23b5cb06b9f
David Guisado Olmedo <davidguiolm@outlook.com>
2024-11-26 01:50:53 +0100

    Subida inicial del proyecto

---
+++ src/main/java/com/davidguiolm/app/lectorama/LectoramaApp.java
@@ -0,0 +1,13 @@
+package com.davidguiolm.app.lectorama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectoramaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectoramaApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidguiolm/app/lectorama/LectoramaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidguiolm.app.lectorama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectoramaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
